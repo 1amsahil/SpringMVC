4e11cdd2a23ec987bffe67da62135f5ed3a73ac2
1amsahil <sahil981156@gmail.com>
2024-11-23 18:58:55 +0530

    MVC till reuestMapping Annotation

---
+++ src/main/java/com/eternity/code/SpringMVC/HomeController.java
@@ -0,0 +1,31 @@
+package com.eternity.code.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home()
+    {
+        System.out.println("Home Method Called ");
+        return "index.jsp";
+    }
+
+//    @RequestMapping("add")
+//    public String add(int num1,int num2, HttpSession session)
+//    {
+//        System.out.println("Add Method called");
+//
+//        int result = num1 + num2;
+//
+//        session.setAttribute("result", result);
+//
+//        return "result.jsp";
+//    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@page language = "java" %>
+
+<html>
+    <body>
+            <h2> Eternity Calculator </h2>
+
+            <form action="add">
+                <label>Enter 1st Number : </label>
+                <input type="text" id="num1" name="num1">
+                <br>
+                <label>Enter 2nd Number : </label>
+                <input type="text" id="num2" name="num2">
+                <br>
+                <input type="submit" value="submit">
+            </form>
+    </body>
+</html>
